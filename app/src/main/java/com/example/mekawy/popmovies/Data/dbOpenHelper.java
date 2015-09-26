ec2a2283be0cdc100ec3677f889780a2d74828c5
Mohamed-mekawy <mekawy2012@gmail.com>
2015-09-25 18:33:38 -0700

    5.3 Refactoring DbHelper

--- app/src/main/java/com/example/mekawy/popmovies/Data/dbOpenHelper.java
+++ app/src/main/java/com/example/mekawy/popmovies/Data/dbOpenHelper.java
@@ -5,78 +5,66 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-import com.example.mekawy.popmovies.Data.dbContract.MOST_POPULAR_TABLE;
-    import com.example.mekawy.popmovies.Data.dbContract.VOTE_TABLE;
-    import com.example.mekawy.popmovies.Data.dbContract.FAVORITE_TABLE;
-
+import com.example.mekawy.popmovies.Data.dbContract.FAV_MOVIES_TABLE;
+import com.example.mekawy.popmovies.Data.dbContract.MOST_VOTED_TABLE;
+import com.example.mekawy.popmovies.Data.dbContract.POP_MOVIES_TABLE;
 
 public class dbOpenHelper extends SQLiteOpenHelper {
 
-    public static final int VERSION_NUMBER=1;
-    public static final String DB_NAME="popmovies.db";
+    private final static String DB_NAME="MoviesApp";
+    private final static int DB_VERSION=1;
+
 
     public dbOpenHelper(Context context) {
-        super(context, DB_NAME, null, VERSION_NUMBER);
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_POP_MOVIES_TABLE=
+                "CREATE TABLE "+ dbContract.POP_MOVIES_TABLE.TABLE_NAME +" ( "+
+                        dbContract.POP_MOVIES_TABLE._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_TAG + " INTEGER UNIQUE NOT NULL, " +
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_TITLE + " TEXT NOT NULL, " +
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_OVERVIEW +" TEXT NOT NULL, " +
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_RELEASE_DATE+ " TEXT NOT NULL, "+
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_POSTER_PATH + " TEXT NOT NULL, "+
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_VOTE_AVERAGE+ " REAL NOT NULL, "+
+                        dbContract.POP_MOVIES_TABLE.OWM_COLUMN_IS_FAVORITE+ " INTEGER NOT NULL);";
 
-        final String SQL_MOVIES_TABLE=
-                        "CREATE TABLE "+ dbContract.MOVIES_TABLE.TABLE_NAME+
-                        " ("+ dbContract.MOVIES_TABLE._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_TAG +" INTEGER UNIQUE NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_TITLE +" TEXT NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_OVERVIEW + " TEXT NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_RELEASE_DATE + " TEXT NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_POSTER_PATH +" TEXT NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_VOTE_AVERAGE +" REAL NOT NULL, "+
-                        dbContract.MOVIES_TABLE.OWM_COLUMN_IS_FAVORITE +" INTEGER NOT NULL );";
-
-        final String SQL_MOST_POP_MOVIES_TABLE=
-                        "CREATE TABLE "+MOST_POPULAR_TABLE.TABLE_NAME+
-                        " ("+MOST_POPULAR_TABLE._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                        MOST_POPULAR_TABLE.OWM_COLUMN_MOVIE_ID + " INTEGER NOT NULL, "+
-                        MOST_POPULAR_TABLE.OWM_COLUMN_TITLE +" TEXT NOT NULL, "+
-                        MOST_POPULAR_TABLE.OWM_COLUMN_IS_FAVORITE+ " INTEGER NOT NULL,"+
-                        " FOREIGN KEY (" +MOST_POPULAR_TABLE.OWM_COLUMN_MOVIE_ID +") REFERENCES "+
-                        dbContract.MOVIES_TABLE.TABLE_NAME+" ("+ dbContract.MOVIES_TABLE._ID +" ), "+
-                        " UNIQUE ("+MOST_POPULAR_TABLE._ID+","+MOST_POPULAR_TABLE.OWM_COLUMN_MOVIE_ID+") ON CONFLICT REPLACE);";
-
-        final String SQL_VOTE_DESC_MOVIES_TABLE=
-                        "CREATE TABLE "+VOTE_TABLE.TABLE_NAME+
-                        " ("+VOTE_TABLE._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                                VOTE_TABLE.OWM_COLUMN_MOVIE_ID + " INTEGER NOT NULL, "+
-                                VOTE_TABLE.OWM_COLUMN_TITLE +" TEXT NOT NULL, "+
-                                VOTE_TABLE.OWM_COLUMN_IS_FAVORITE+ " INTEGER NOT NULL,"+
-                        " FOREIGN KEY (" +VOTE_TABLE.OWM_COLUMN_MOVIE_ID +") REFERENCES "+
-                        dbContract.MOVIES_TABLE.TABLE_NAME+" ("+ dbContract.MOVIES_TABLE._ID +" ), "+
-                        " UNIQUE ("+VOTE_TABLE._ID+","+VOTE_TABLE.OWM_COLUMN_MOVIE_ID+") ON CONFLICT REPLACE);";
+        final String SQL_VOTE_MOVIES_TABLE=
+                "CREATE TABLE "+ dbContract.MOST_VOTED_TABLE.TABLE_NAME +" ( "+
+                        dbContract.MOST_VOTED_TABLE._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_TAG + " INTEGER UNIQUE NOT NULL, " +
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_TITLE + " TEXT NOT NULL, " +
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_OVERVIEW +" TEXT NOT NULL, " +
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_RELEASE_DATE+ " TEXT NOT NULL, "+
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_POSTER_PATH + " TEXT NOT NULL, "+
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_VOTE_AVERAGE+ " REAL NOT NULL, "+
+                        dbContract.MOST_VOTED_TABLE.OWM_COLUMN_IS_FAVORITE+ " INTEGER NOT NULL);";
 
         final String SQL_FAV_MOVIES_TABLE=
-                        "CREATE TABLE "+FAVORITE_TABLE.TABLE_NAME+
-                        " ("+FAVORITE_TABLE._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                        FAVORITE_TABLE.OWM_COLUMN_MOVIE_ID + " INTEGER NOT NULL, "+
-                        FAVORITE_TABLE.OWM_COLUMN_TITLE +" TEXT NOT NULL, "+
-                        " FOREIGN KEY (" +FAVORITE_TABLE.OWM_COLUMN_MOVIE_ID +") REFERENCES "+
-                        dbContract.MOVIES_TABLE.TABLE_NAME+" ("+ dbContract.MOVIES_TABLE._ID +" ), "+
-                        " UNIQUE ("+FAVORITE_TABLE._ID+","+FAVORITE_TABLE.OWM_COLUMN_MOVIE_ID+") ON CONFLICT REPLACE);";
+                "CREATE TABLE "+ FAV_MOVIES_TABLE.TABLE_NAME +" ( "+
+                        FAV_MOVIES_TABLE._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                        FAV_MOVIES_TABLE.OWM_COLUMN_TAG + " INTEGER UNIQUE NOT NULL, " +
+                        FAV_MOVIES_TABLE.OWM_COLUMN_TITLE + " TEXT NOT NULL, " +
+                        FAV_MOVIES_TABLE.OWM_COLUMN_OVERVIEW +" TEXT NOT NULL, " +
+                        FAV_MOVIES_TABLE.OWM_COLUMN_RELEASE_DATE+ " TEXT NOT NULL, "+
+                        FAV_MOVIES_TABLE.OWM_COLUMN_POSTER_PATH + " TEXT NOT NULL, "+
+                        FAV_MOVIES_TABLE.OWM_COLUMN_VOTE_AVERAGE+ " REAL NOT NULL);";
 
-
-
-        sqLiteDatabase.execSQL(SQL_MOST_POP_MOVIES_TABLE);
-        sqLiteDatabase.execSQL(SQL_VOTE_DESC_MOVIES_TABLE);
+        sqLiteDatabase.execSQL(SQL_POP_MOVIES_TABLE);
         sqLiteDatabase.execSQL(SQL_FAV_MOVIES_TABLE);
-        sqLiteDatabase.execSQL(SQL_MOVIES_TABLE);
+        sqLiteDatabase.execSQL(SQL_VOTE_MOVIES_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ dbContract.MOVIES_TABLE.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ dbContract.FAVORITE_TABLE.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ dbContract.VOTE_TABLE.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ dbContract.MOST_POPULAR_TABLE.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ MOST_VOTED_TABLE.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ FAV_MOVIES_TABLE.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ POP_MOVIES_TABLE.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
