d8589139329d66b8cd3a095f0bdf416e90d011d8
Mohamed mekawy <mekawy2012@gmail.com>
2015-10-01 12:08:38 -0700

    Update movies_api_key.java

--- app/src/main/java/com/example/mekawy/popmovies/movies_api_key.java
+++ app/src/main/java/com/example/mekawy/popmovies/movies_api_key.java
@@ -1,8 +1,7 @@
 package com.example.mekawy.popmovies;
 
 public enum movies_api_key {
-    API_KEY ("7b5e30851a9285340e78c201c4e4ab99");
-
+API_KEY ("Hiden");//Put yout generated Key here
     private final String value;
     private movies_api_key(String sData){
     value=sData;
