661a43671067d192c8ce669ac8f45ede0f455e05
Mohamed mekawy <mekawy2012@gmail.com>
2015-10-08 05:33:59 -0700

    Update movies_api_key.java

--- app/src/main/java/com/example/mekawy/popmovies/movies_api_key.java
+++ app/src/main/java/com/example/mekawy/popmovies/movies_api_key.java
@@ -2,7 +2,7 @@ package com.example.mekawy.popmovies;
 
 public enum movies_api_key {
     // You can get your key from themoviedb.org
-    API_KEY ("7b5e30851a9285340e78c201c4e4ab99");
+    API_KEY ("XXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
     private final String value;
     private movies_api_key(String sData){
